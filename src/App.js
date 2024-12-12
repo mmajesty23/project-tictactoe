a956f780996d802c282bc70a2d745a3af7e47db3
mmajesty23 <muhammadmajesty23@gmail.com>
2024-12-12 17:25:08 +0700

    Restarting build the project

--- src/App.js
+++ src/App.js
@@ -1,36 +1,40 @@
-import "./App.css";
+import { useState } from "react";
 
 function Square() {
-  return <div className="border border-black px-6 py-6 inline-block"></div>;
+  return (
+    <button className="border border-gray-400 float-left text-lg font-bold text-center -mr-[1px] -mb-[1px] w-9 h-9">
+      X
+    </button>
+  );
 }
 
 function Board() {
   return (
-    <div className="max-w-max mx-auto">
-      <div>
+    <>
+      <div className="board-row">
         <Square />
         <Square />
         <Square />
       </div>
-      <div className="-mt-[7px]">
+      <div className="board-row">
         <Square />
         <Square />
         <Square />
       </div>
-      <div className="-mt-[7px]">
+      <div className="board-row">
         <Square />
         <Square />
         <Square />
       </div>
-    </div>
+    </>
   );
 }
 
 function App() {
   return (
-    <div>
+    <>
       <Board />
-    </div>
+    </>
   );
 }
 
